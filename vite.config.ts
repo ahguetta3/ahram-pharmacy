d98426c5243329a5c8e0fd49aa357420219748fa
ahguetta3 <ahmdabidine3390@gmail.com>
2026-03-16 04:57:03 +0100

    fix: CSP and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2020',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    }
+  }
 })
